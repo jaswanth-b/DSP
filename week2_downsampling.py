9247de39f50a913909c919f3a01c33eb43940d2a
jaswanth-b <158155762+jaswanth-b@users.noreply.github.com>
2024-08-26 20:59:08 +0530

    Add files via upload

---
+++ week2_downsampling.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import matplotlib.pyplot as pt
+from scipy.io import wavfile
+fs,x=wavfile.read("Classical.wav")
+def downsampling(fs,x):
+    factor=int(input("Enter upsampling factor"))
+    y=np.zeros(int(len(x)/factor))
+    for i in range(int(len(x)/factor)):
+        if(i%factor==0):
+            y[i]=x[i]
+        else:
+            pass
+    return fs,y
+Fs,Y=downsampling(fs,x)
+wavfile.write("downsampled.wav",Fs,Y)
